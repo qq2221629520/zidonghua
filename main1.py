f94ce988fe9557cc1a15ec967a333da66c055411
fangzhongyuan <qq2221629520@gmail.com>
2023-03-03 11:19:15 +0800

    增加了

--- main1.py
+++ main1.py
@@ -1,1 +1,92 @@
-# 实现自动换背景# E:\anaconda3\python.exe -m pip install -i https://pypi.tuna.tsinghua.edu.cn/simple py2exe# 打包命令 pyinstaller xxxxxxxx --onefile --icon=xxxxxxxxxx# pyinstaller --onedir your_program.py# 生成的文件夹中会包含一个your_program可执行文件和一个your_program_libs文件夹，其中包含了所有的依赖库和资源文件。您可以将your_program文件复制到任何地方，并在运行时将your_program_libs文件夹放在同一目录下。# pyinstaller --onefile --add-data 'path/to/library;library' your_program.py# 这将打包your_program.py及其依赖库和资源文件到一个可执行文件中，并将path/to/library文件夹打包为一个名为library的文件夹。在运行时，您可以将library文件夹放在任何地方，并在运行可执行文件时使用os.path.join(sys._MEIPASS, 'library')获取它们的绝对路径。import requestsimport base64import tkinter as tkimport tkinter.filedialogroot = tk.Tk()root.title("选择照片")root.geometry("150x150")root.resizable(0, 0)def select():    global filename    filename = tk.filedialog.askopenfilename()    if filename:        print(filename)button = tk.Button(root, text="选择照片", command=select)button.place(x=50, y=50)root.mainloop()'''人像分割'''request_url = "https://aip.baidubce.com/rest/2.0/image-classify/v1/body_seg"# 二进制方式打开图片文件f = open(filename, 'rb')img = base64.b64encode(f.read())params = {"image":img,             # "type":"foreground"          }access_token = '24.afd406e092e60de0574169387de9d9dc.2592000.1680172187.282335-30828139'request_url = request_url + "?access_token=" + access_tokenheaders = {'content-type': 'application/x-www-form-urlencoded'}response = requests.post(request_url, data=params, headers=headers,)if response:    #print (response.json())    # 保存图片，原来的照片名字后面加上“_new”，在同一个文件夹下    with open(filename[:-4] + "_new.jpg", 'wb') as f:        f.write(base64.b64decode(response.json()['foreground']))        f.close()    print("生成新图片！")else:    print ("请求失败！")
+# 实现自动抠出人像
+# E:\anaconda3\python.exe -m pip install -i https://pypi.tuna.tsinghua.edu.cn/simple py2exe
+# 打包命令 pyinstaller E:\pythonxiangmu\zidonghua\main1.py --onefile --icon=tubiao.ico
+# pyinstaller --onedir E:\pythonxiangmu\zidonghua\main1.py
+# 生成的文件夹中会包含一个your_program可执行文件和一个your_program_libs文件夹，其中包含了所有的依赖库和资源文件。您可以将your_program文件复制到任何地方，并在运行时将your_program_libs文件夹放在同一目录下。
+# pyinstaller --onefile --add-data 'path/to/library;library' your_program.py
+# 这将打包your_program.py及其依赖库和资源文件到一个可执行文件中，并将path/to/library文件夹打包为一个名为library的文件夹。在运行时，您可以将library文件夹放在任何地方，并在运行可执行文件时使用os.path.join(sys._MEIPASS, 'library')获取它们的绝对路径。
+import time
+
+import requests
+import base64
+
+import tkinter as tk
+import tkinter.filedialog
+
+from PIL import Image
+
+def select():
+    global filename
+    filename = tk.filedialog.askopenfilename()
+    if filename:
+        print(filename)
+
+root = tk.Tk()
+root.title("选择照片")
+root.geometry("150x800")
+root.resizable(0, 0)
+def select():
+    global filename
+    filename = tk.filedialog.askopenfilename()
+    if filename:
+        print(filename)
+
+
+def kouchurenxiang():
+    request_url = "https://aip.baidubce.com/rest/2.0/image-classify/v1/body_seg"
+    # 二进制方式打开图片文件
+    global filename
+    f = open(filename, 'rb')
+    img = base64.b64encode(f.read())
+    params = {"image": img,
+              # "type":"foreground"
+              }
+    access_token = '24.afd406e092e60de0574169387de9d9dc.2592000.1680172187.282335-30828139'
+    request_url = request_url + "?access_token=" + access_token
+    headers = {'content-type': 'application/x-www-form-urlencoded'}
+    response = requests.post(request_url, data=params, headers=headers, )
+    if response:
+        # print (response.json())
+        # 保存图片，原来的照片名字后面加上“new”，在同一个文件夹下
+        with open(filename[:-4] + "new.png", 'wb') as f:
+            f.write(base64.b64decode(response.json()['foreground']))
+            f.close()
+        filename = filename[:-4] + "new.png"
+        print("生成新图片！")
+        time.sleep(1)
+        print("5！")
+        time.sleep(1)
+        print("4！")
+        time.sleep(1)
+        print("3！")
+        time.sleep(1)
+        print("2！")
+        time.sleep(1)
+        print("1！")
+        #退出程序
+        root.destroy()
+
+
+    else:
+        print("请求失败！")
+        time.sleep(1)
+        print("5！")
+        time.sleep(1)
+        print("4！")
+        time.sleep(1)
+        print("3！")
+        time.sleep(1)
+        print("2！")
+        time.sleep(1)
+        print("1！")
+
+button1 = tk.Button(root, text="选择照片", command=select)
+button1.place(x=50, y=50)
+button1_1 = tk.Button(root, text="抠出人像", command=kouchurenxiang)
+button1_1.place(x=50, y=100)
+root.mainloop()
+
+
+
+
+
--- main3.py
+++ main3.py
@@ -1,1 +1,152 @@
-# 请输入这个程序的功能 # pip install -i https://pypi.tuna.tsinghua.edu.cn/simple xxxx # 打包命令 pyinstaller xxxxxxxx --onefile --icon=xxxxxxxxxx #
+# 实现chatgpt对话机器人
+# pip命令：      E:\anaconda3\python.exe -m pip install -i https://pypi.tuna.tsinghua.edu.cn/simple openai
+# 打包命令1：     pyinstaller E:\pythonxiangmu\zidonghua\main3.py --onefile --icon=tubiao.ico --noconsole
+# 打包命令2：     pyinstaller --onedir your_program.py
+#                生成的文件夹中会包含一个your_program可执行文件和一个your_program_libs文件夹，其中包含了所有的依赖库和资源文件。您可以将your_program文件复制到任何地方，并在运行时将your_program_libs文件夹放在同一目录下。
+# 打包命令3：     pyinstaller --onefile --add-data 'path/to/library;library' your_program.py
+#                这将打包your_program.py及其依赖库和资源文件到一个可执行文件中，并将path/to/library文件夹打包为一个名为library的文件夹。在运行时，您可以将library文件夹放在任何地方，并在运行可执行文件时使用os.path.join(sys._MEIPASS, 'library')获取它们的绝对路径。
+
+
+
+import tkinter as tk
+import openai
+import time
+
+SECRET_KEY = "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"  # 替换你的API key
+
+openai.api_key = SECRET_KEY
+ENGINE_LIST = openai.Engine.list()
+MODEL_LIST = openai.Model.list()
+MAX_TOKEN_LEN = 4000
+TIME_OUT = 3
+
+BOT_ROLE = 'assistant'
+USER_ROLE = 'user'
+
+
+# 创建机器人
+class MyChatBot:
+    def __init__(self) -> None:
+        self.messages = []
+        self.reset_log()
+
+    def receive_message_from_api(self):
+        response = []
+        try:
+            response = openai.ChatCompletion.create(
+                model='gpt-3.5-turbo-0301',
+                messages=self.messages,
+                temperature=1.0,
+                max_tokens=MAX_TOKEN_LEN,
+                top_p=0.6,
+                frequency_penalty=2.0,
+                presence_penalty=0.0,
+                stream=True,
+                timeout=TIME_OUT,
+            )
+        except:
+            pass
+        return response
+
+    def get_response(self, prompt):
+        self.add_user_content(prompt)
+        stream_response = self.receive_message_from_api()
+
+        return stream_response
+
+    def reset_log(self):
+        self.messages = [{'role': 'system', 'content': f'you are {BOT_ROLE}, a cute catgirl.'}]
+
+    def add_user_content(self, content):
+        self.messages.append({'role': USER_ROLE, 'content': content})
+
+    def add_bot_content(self, content):
+        self.messages.append({'role': BOT_ROLE, 'content': content})
+
+
+class ChatUI:
+    def __init__(self):
+        chatbot = MyChatBot()
+
+        # 创建 GUI 界面
+        root = tk.Tk()
+        root.title('Chatbot')
+        root.attributes("-topmost", True)
+
+        # 创建对话框
+        conversation = tk.Text(root, bd=1, bg='white', width=50, height=30)
+        conversation.config(state='disabled')
+        conversation.grid(row=0, column=0, sticky='nsew', padx=5, pady=5)
+
+        # 创建输入框
+        input_field = tk.Entry(root, width=50)
+        input_field.bind("<Return>", (lambda event: self.chatting()))  # 使用回车发送
+        input_field.grid(row=1, column=0, sticky='nsew', padx=5)
+
+        # 创建发送按钮
+        send_button = tk.Button(root, text='Send', command=self.chatting)  # 点击发送
+        send_button.grid(row=1, column=1, sticky='nsew', padx=5)
+
+        # 创建清除对话按钮
+        clear_button = tk.Button(root, text='Clear', command=self.clear_conversation)  # 点击发送
+        clear_button.grid(row=0, column=1, sticky='nsew', padx=5)
+
+        self.chatbot = chatbot
+        self.root = root
+        self.conversation = conversation
+        self.input_field = input_field
+
+    def clear_conversation(self):
+        self.chatbot.reset_log()
+        self.conversation.config(state='normal')
+        self.conversation.delete('1.0', tk.END)
+        self.conversation.config(state='disabled')
+
+    # 定义获取响应的函数
+    def chatting(self):
+
+        self.conversation.config(state='normal')
+        input_text = self.input_field.get()
+        self.conversation.insert(tk.END, "You: " + input_text + '\n')
+        self.conversation.see(tk.END)
+
+        self.input_field.delete(0, tk.END)  # 清空输入bar
+        self.input_field.update()
+
+        self.conversation.insert(tk.END, "Bot: ")  # 开始导出回答
+        stream_response = self.chatbot.get_response(input_text)
+        answer = ""
+        timeout_cnt = 0
+        while True:  # 连续获取stream，并保存最终的answer
+            try:
+                package = next(stream_response)
+                if hasattr(package.choices[0].delta, 'role'):
+                    continue
+                single_token = package.choices[0].delta.content
+
+                self.conversation.insert(tk.END, single_token)
+                self.conversation.see(tk.END)
+                self.conversation.update()
+
+                answer += single_token
+                if package.choices[0].finish_reason == "stop":
+                    break
+            except:
+                if len(answer) > 0:
+                    break
+                timeout_cnt += 1
+                if timeout_cnt >= TIME_OUT:
+                    self.conversation.insert(tk.END, "[Timeout In Stream]")
+                    self.conversation.see(tk.END)
+                    break
+                time.sleep(1)
+
+        self.chatbot.add_bot_content(answer)  # 记录机器人的回答
+        self.conversation.insert(tk.END, '\n')
+        self.conversation.see(tk.END)
+        self.conversation.config(state='disabled')
+
+
+chat_ui = ChatUI()
+chat_ui.root.mainloop()
+# 代码结束
